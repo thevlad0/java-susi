e54263037f04d33a09fa1fc6c2fe81b558c65d71
Vladimir Velikov <the_vlad0@icloud.com>
2024-10-15 14:55:05 +0300

    add course logic

---
+++ src/CourseHandler/Course.java
@@ -0,0 +1,39 @@
+package CourseHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+    private String name;
+    private String headTeacher;
+    private List<String> atendants = new ArrayList<>();
+    private String description;
+
+    public Course(String name, String headTeacher, String description) {
+        this.name = name;
+        this.headTeacher = headTeacher;
+        this.description = description;
+    }
+
+    public void addAtendant(String atendant) {
+        atendants.add(atendant);
+    }
+
+    public void removeAtendant(String atendant) {
+        atendants.remove(atendant);
+    }
+
+    public List<String> getAtendants() {
+        return atendants;
+    }
+
+    public void assignHeadTeacher(String headTeacher) {
+        this.headTeacher = headTeacher;
+    }
+
+    public void displayCourseInfo() {
+        System.out.printf("Course name: %s%n", name);
+        System.out.printf("Head teacher: %s%n", headTeacher);
+        System.out.printf("Description: %s%n", description);
+    }
+}
---
+++ src/CourseHandler/CourseHandler.java
@@ -0,0 +1,32 @@
+package CourseHandler;
+
+import db.DataHandler;
+
+import java.util.List;
+
+public class CourseHandler {
+    private static int courseID = 0;
+
+    private CourseHandler() {}
+
+    public static void createCourse(String name, String headTeacher, String description) {
+        DataHandler.writeData(String.valueOf(courseID++), new Course(name, headTeacher, description));
+    }
+
+    public static List<String> getCourseAttendants(int courseId) {
+        return DataHandler.getCoursesList().get(courseId).getAtendants();
+    }
+
+    public static void deleteCourse(int courseId) {
+        DataHandler.getCoursesList().remove(courseId);
+    }
+
+    public static void addAtendant(int courseId, String username) {
+        DataHandler.getCoursesList().get(courseId).addAtendant(username);
+    }
+
+    public static void deleteAtendant(int courseId, String username) {
+        DataHandler.getCoursesList().get(courseId).removeAtendant(username);
+    }
+
+}
